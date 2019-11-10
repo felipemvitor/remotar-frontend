d1d7439e594472a329870d64782e44a92a87e5db
felipemvitor <felipe.martinsvitor@gmail.com>
2019-11-10 18:10:37 -0300

    Socket url correction.

--- src/socket/Socket.js
+++ src/socket/Socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-const SOCKET_URL = process.env.SOCKET_URL
+const SOCKET_URL = process.env.SOCKET_URL || 'http://localhost:4500'
 
 export const socket = io(SOCKET_URL, { query: "type=remotar" })
